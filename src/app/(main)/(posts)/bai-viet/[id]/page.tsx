65522a2518b16c747a8a912dee8056e5f5543c9b
CuongDangDev04 <cuongdang.270920@gmail.com>
2025-07-29 07:08:44 +0700

    fix: bug param await on PostPage

--- src/app/(main)/(posts)/bai-viet/[id]/page.tsx
+++ src/app/(main)/(posts)/bai-viet/[id]/page.tsx
@@ -1,15 +1,16 @@
 import PostDetail from "@/components/posts/PostDetail";
-interface PostPageProps {
-    params: {
-        id: string;
-    };
-}
-export default async function PostPage({ params }: PostPageProps) {
-    const postId = Number( params.id);
 
-    return (
-        <div className="w-full md:w-2/3 mx-auto py-6">
-            <PostDetail postId={postId} />
-        </div>
-    );
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;  
+  const postId = Number(id);
+
+  return (
+    <div className="w-full md:w-2/3 mx-auto py-6">
+      <PostDetail postId={postId} />
+    </div>
+  );
 }
