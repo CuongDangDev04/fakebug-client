bf3fc8aa48e26da2d762973696fd61063c915fff
CuongDangDev04 <cuongdang.270920@gmail.com>
2025-08-07 07:03:16 +0700

    feat: add img seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,22 @@ export const metadata = {
   description: 'Mạng xã hội FakeBug',
   icons: {
     icon: '/lg.png',
+  }, 
+  openGraph: {
+    title: 'FakeBug - Mạng xã hội',
+    description: 'Kết nối với bạn bè và chia sẻ khoảnh khắc thú vị.',
+    url: 'https://fakebug.vercel.app',
+    siteName: 'FakeBug',
+    images: [
+      {
+        url: 'https://fakebug.vercel.app/lg.png', 
+        width: 1200,
+        height: 630,
+        alt: 'FakeBug Preview',
+      },
+    ],
+    locale: 'vi_VN',
+    type: 'website',
   },
   other: {
     'google-site-verification': '8cpVrNgWjVy86NmKpzl7yQRiVAx3WXPfKkcjFYgq7N8',
